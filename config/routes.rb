4017b449e904f5939900b2a886a585eedce6c98d
EC2 Default User <ec2-user@ip-172-31-86-83.ec2.internal>
2022-03-04 01:28:36 +0000

    {update}ジャンル編集画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :admin do
-    resources :genres, only: [:index, :create, :edit]
+    resources :genres, only: [:index, :create, :edit, :update]
   end
   namespace :admin do
     resources :customers, only: [:index, :show, :edit, :update]
