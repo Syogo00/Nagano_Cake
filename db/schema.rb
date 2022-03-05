2ed4184c80f39e5e4a7803a8e77cc71db3729a33
EC2 Default User <ec2-user@ip-172-31-86-83.ec2.internal>
2022-03-05 02:51:26 +0000

    {update}itemsテーブル、is_activeにdefault値追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_28_150348) do
+ActiveRecord::Schema.define(version: 2022_03_05_014753) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -96,7 +96,7 @@ ActiveRecord::Schema.define(version: 2022_02_28_150348) do
     t.string "name"
     t.text "introduction"
     t.integer "price"
-    t.boolean "is_active"　
+    t.boolean "is_active", default: true
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
