ca2212da820fb126f4f96fcdd37d649df3f5abe5
EC2 Default User <ec2-user@ip-172-31-86-83.ec2.internal>
2022-03-14 15:22:35 +0000

    {update}homt,top画面編集

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,14 +6,18 @@ class Public::OrdersController < ApplicationController
 
   def confirm
     @order = Order.new(order_params)
+    @cart_items = current_customer.cart_items
     if params[:order][:select_address] == "0"
       @order.delivery_postal = current_customer.postal_code
       @order.delivery_address = current_customer.address
       @order.delivery_name = current_customer.first_name + current_customer.last_name
     elsif params[:order][:select_address] == "1"
-      @address = Address.find(params[:order][:address_id])
+      @order = Address.find(params[:order][:address_id])
+      @order.delivery_postal = @order.postal_code
+      @order.delivery_address = @order.address
+      @order.delivery_name = @order.name
     else
-      @address = Order.new(order_params)
+
     end
     # binding.pry
   end
